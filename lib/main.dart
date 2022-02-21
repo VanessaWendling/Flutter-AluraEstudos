6aee1b2319bf841d707c0a417b6ffb400d41a467
VanessaWendling <vanessamw2001@hotmail.com>
2022-02-21 11:32:44 -0300

    Parte da tela de adicionar transferências

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,63 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
         home:Scaffold(
-          appBar: AppBar(title: Text('Transferências'),),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {  },
-            child: Icon(Icons.add),
-          ),
-          body: ListaTransferencia(),
+          body: FormularioTransferencia(),
         )
-      )
     );
+  }
+
+}
+
+
+
+class FormularioTransferencia extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Criando Transferência'),),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+              decoration: InputDecoration(
+                labelText: 'Número da conta',
+                hintText: '0000',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: 'Valor',
+                hintText: '0000',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () => {},
+              child: Text('Confirmar'))
+        ],
+      ),
+    );
+  }
+}
 
 /*
 StatefulWidget - capacidade de modificar o widget de maneira dinâmica.
@@ -22,12 +68,19 @@ class ListaTransferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Column(
-      children: <Widget>[
-        ItemTransferencia(Transferencia(100.0, 1000)),
-        ItemTransferencia(Transferencia(200.0, 1001)),
-        ItemTransferencia(Transferencia(300.0, 2200)),
-      ],
+    return Scaffold(
+      appBar: AppBar(title: Text('Transferências'),),
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(100.0, 1000)),
+          ItemTransferencia(Transferencia(200.0, 1001)),
+          ItemTransferencia(Transferencia(300.0, 2200)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
@@ -58,3 +111,4 @@ class Transferencia{
 
   Transferencia(this.valor, this.numeroConta);
 }
+
