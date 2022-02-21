2906b1a2618f93d6ca4087728ae08b31d40707dd
VanessaWendling <vanessamw2001@hotmail.com>
2022-02-21 10:51:16 -0300

    Componentização e refatoração do código

--- lib/main.dart
+++ lib/main.dart
@@ -8,24 +8,53 @@ void main() => runApp(
             onPressed: () {  },
             child: Icon(Icons.add),
           ),
-          body: Column(
-            children: <Widget>[
-              Card(
-                child:ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('100.0'),
-                  subtitle: Text('1000'),
-                ),
-              ),
-              Card(
-                child:ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('200.0'),
-                  subtitle: Text('1000'),
-                ),
-              ),
-            ],
-          )
+          body: ListaTransferencia(),
         )
       )
     );
+
+/*
+StatefulWidget - capacidade de modificar o widget de maneira dinâmica.
+StatelessElement - não muda dinamicamente.
+ */
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100.0, 1000)),
+        ItemTransferencia(Transferencia(200.0, 1001)),
+        ItemTransferencia(Transferencia(300.0, 2200)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget{
+
+  //o _ indica que o atributo é privado
+  final Transferencia _transferencia;
+
+
+  ItemTransferencia(this._transferencia);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia{
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
